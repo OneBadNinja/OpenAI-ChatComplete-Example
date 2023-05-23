2ed748384221d355ce595dbf45a45c0685786277
OneBadNinja <itsjoeylag@gmail.com>
2023-05-23 18:28:37 +0200

    Add handler for trimming history when maximum token size exceeded. Add helper messages when rate limit or maximum token size is exceeded

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import lorem
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
@@ -9,7 +10,7 @@ openai.api_key = api_key
 
 def get_system_message():
     return {"role": "system",
-            "content": f"""You are an overly-friendly chatbot. You prefix all your answers with 'Yo broski'"""}
+            "content": f"""You are an overly-friendly chatbot. You always reply in English Pirate!"""}
 
 def get_user_message(prompt):
     return {"role": "user",
@@ -33,19 +34,28 @@ def get_gpt3_response(prompt, history):
 
 history = []
 
+
+
 while True:
     prompt = input("Your prompt: ")
+
     prompt = get_user_message(prompt)
-    response = get_gpt3_response(prompt, history)
 
-    message_resp = list(response["choices"])[0].to_dict()["message"]
-    print(f'Reply: {message_resp["content"]}')
-    print(f'Usage: {response["usage"]}')
-    print("\n\n")
+    try:
+        response = get_gpt3_response(prompt, history)
 
-    # Record history to persist context
-    history.append(prompt)
-    history.append(message_resp)
+        message_resp = list(response["choices"])[0].to_dict()["message"]
+        print(f'Reply: {message_resp["content"]}')
+        print(f'Usage: {response["usage"]}')
+        print("\n\n")
 
-    # The above causes an issue however because once I exceed the maximum token size, it begins
-    # to cut off the context, which includes the system message first!!!
+        # Record history to persist context
+        history.append(prompt)
+        history.append(message_resp)
+    except (openai.error.InvalidRequestError, openai.error.RateLimitError) as e:
+        if(isinstance(e, openai.error.RateLimitError)):
+            print("Rate limit exceeded. You are allowed 3 messages/min. Please try again in a few seconds")
+        elif(isinstance(e, openai.error.InvalidRequestError)):
+            print("Maximum token size exceeded for the API call (4097 tokens for gpt-3.5-turbo). Trimming the earliest message and attempting again.")
+            history = history[1:] # Trim the first message
+
